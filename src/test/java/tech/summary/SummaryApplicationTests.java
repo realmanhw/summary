b98c0d1f25a6ecb502d8b6b20954854530d1ae4e
huangwei01 <huangwei01@itiger.com>
2020-12-12 01:28:37 +0800

    origin

---
+++ src/test/java/tech/summary/SummaryApplicationTests.java
@@ -0,0 +1,12 @@
+package tech.summary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummaryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
